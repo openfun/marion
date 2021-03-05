b415f3b05e392e24e731a015d3822a200abd9311
Julien Maupetit <julien@maupetit.net>
2021-03-05 18:41:09 +0100

    🐛(certificates) add weasyprint fetcher for static files
    When static files are integrated in a certificate template, weasyprint
    was not able to access and render them as the template suppose that they
    are served locally with source URLs starting with the
    settings.STATIC_URL path.
    
    We now fetch files from the configured storage instead.

---
+++ src/marion/marion/certificates/tests/test_utils.py
@@ -0,0 +1,43 @@
+"""Tests for the marion.certificates.utils module"""
+
+from pathlib import Path
+
+from django.conf import settings
+
+from marion.certificates.utils import static_file_fetcher
+
+
+# pylint: disable=invalid-name
+def test_static_file_fetcher(fs):
+    """Test weasyprint custom static file fetcher."""
+
+    # Create a fake static file for the marion app
+    relative_static_file_path = "marion/test.txt"
+    static_file_path = Path(
+        f"{settings.BASE_DIR}/marion/certificates/static/{relative_static_file_path}"
+    )
+    fs.create_file(static_file_path, contents="This is content.")
+
+    # Fetch this file
+    data = static_file_fetcher(
+        f"file://{settings.STATIC_URL}{relative_static_file_path}"
+    )
+    assert data.get("filename") == static_file_path.name
+    assert data.get("mime_type") == "text/plain"
+    file_ = data.get("file_obj")
+    assert file_.readlines() == [
+        b"This is content.",
+    ]
+    file_.close()
+
+    # Test Weasyprint fallback (if the file is not located in a Django static file path)
+    file_path = "/tmp/foo.txt"
+    fs.create_file(file_path, contents="This is a different content.")
+
+    data = static_file_fetcher(f"file://{file_path}")
+    assert data.get("mime_type") == "text/plain"
+    file_ = data.get("file_obj")
+    assert file_.readlines() == [
+        b"This is a different content.",
+    ]
+    file_.close()
