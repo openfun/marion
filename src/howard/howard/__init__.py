9e125b7e474e18f038b1018161440eed9161dec9
Julien Maupetit <julien@maupetit.net>
2021-03-26 15:12:30 +0100

    🔖(minor) bump howard release to 0.1.0
    Added:
    
    - Draft realisation certificate issuer

---
+++ src/howard/howard/__init__.py
@@ -0,0 +1,23 @@
+"""Howard, FUN documents."""
+
+import importlib.metadata
+from pathlib import Path
+
+from setuptools.config import read_configuration
+
+
+def _get_version():
+    """Get version from installed package with a fallback to the setup.cfg version
+    string.
+
+    """
+
+    try:
+        return importlib.metadata.version("marion-howard")
+    except importlib.metadata.PackageNotFoundError:
+        return read_configuration(Path(__file__).parent / ".." / "setup.cfg")[
+            "metadata"
+        ]["version"]
+
+
+__version__ = _get_version()
